bd4cf3b01d408c9f4a53f191a4c74101bda07a05
Elise Phan <aliceconnic@gmail.com>
2019-04-23 21:29:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,13 +8,9 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer); /* code change */
+const store = createStore(
+    shoppingListItemReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); /* code change */
  
-ReactDOM.render(
-    <Provider store={store}>
-        {' '}
-        /* code change */
-        <App />
-    </Provider> /* code change */,
-    document.getElementById('root')
-);
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
